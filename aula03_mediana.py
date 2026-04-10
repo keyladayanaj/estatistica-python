d62cc7b71cb20de60d9f36479ccb9c0775e04e14
Keyla Dayana <keyladayanaji@gmail.com>
2026-04-10 20:22:23 -0300

    Aula 03 - Mediana

---
+++ aula03_mediana.py
@@ -0,0 +1,14 @@
+# Aula 03 - Mediana
+import numpy as np
+
+conjunto_A = [10, 13, 11, 10, 87]
+conjunto_B = [23, 31, 17, 18]
+
+# np.median ordena os valores e pega o do meio
+print(f'Mediana do conjunto A: {np.median(conjunto_A)}')
+print(f'Média do conjunto A: {np.mean(conjunto_A)}')
+
+print(f'Mediana do conjunto B: {np.median(conjunto_B)}')
+print(f'Média do conjunto B: {np.mean(conjunto_B)}')
+
+
